28db8fc111e748bdfb202e505b114e675a12b4b2
Sargun Vohra <sargun.vohra@gmail.com>
2023-02-11 13:10:05 -0800

    refactor

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,19 @@ import { Readable } from "node:stream"
 
 import execa from "execa"
 import splitLines from "split-lines"
+import * as t from "typanion"
 
-export const gitCredentialFill = async (params: {
-  host: string
-  protocol: string
-  path?: string
-}): Promise<Record<string, string>> => {
-  const inputLines = [`host=${params.host}`, `protocol=${params.protocol}`]
-  if (params.path) inputLines.push(`path=${params.path}`)
+const runGitCredentialCommand = async (
+  subcommand: `fill` | `approve` | `reject`,
+  inputEntries: Record<string, string>,
+) => {
+  const inputLines = Object.entries(inputEntries)
+    .filter(([_key, value]) => value !== null && value !== undefined)
+    .map(([key, value]) => `${key}=${value}`)
+
   const input = `${inputLines.join(`\n`)}\n`
 
-  const execPromise = execa(`git`, [`credential`, `fill`], {
+  const execPromise = execa(`git`, [`credential`, subcommand], {
     env: {
       // If no credentials are stored, we want to error out, but git will attempt to
       // prompt instead. GIT_ASKPASS is a variable that tells git which program to
@@ -26,12 +28,27 @@ export const gitCredentialFill = async (params: {
 
   const { stdout } = await execPromise
 
-  const entries = splitLines(stdout).reduce((acc, line) => {
-    // use a regex with a capturing group to ensure a single split
-    // in case there's an = in the value
-    const [key, value] = line.split(/=(.*)/s)
-    return acc.set(key, value)
-  }, new Map<string, string>())
+  return Object.fromEntries(
+    splitLines(stdout).reduce((acc, line) => {
+      // use a regex with a capturing group to ensure a single split
+      // in case there's an = in the value
+      const [key, value] = line.split(/=(.*)/s)
+      return acc.set(key, value)
+    }, new Map<string, string>()),
+  )
+}
 
-  return Object.fromEntries(entries)
+export const gitCredentialFill = async (params: {
+  host: string
+  protocol: string
+  path?: string
+}) => {
+  return t.as(
+    await runGitCredentialCommand(`fill`, params),
+    t.isPartial({
+      username: t.isString(),
+      password: t.isString(),
+    }),
+    { errors: true, throw: true },
+  )
 }
